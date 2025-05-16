8595e16075d53d0c2793c5522c5245fb4adca9e6
Davi de Carvalho <davi_carvalho96@hotmail.com>
2025-05-16 16:35:17 -0300

    arrumando lint

--- app/jobs/teste.ts
+++ app/jobs/teste.ts
@@ -7,13 +7,13 @@ export default class TesteJob implements JobHandlerContract<TestePayload> {
    * Base Entry point
    */
   async handle(job: Job<TestePayload>) {
-    console.log('TesteJob')
+    console.log('TesteJob', job)
   }
 
   /**
    * This is an optional method that gets called if it exists when the retries has exceeded and is marked failed.
    */
   async failed(job: Job<TestePayload>) {
-    console.log('TesteJob failed')
+    console.log('TesteJob failed', job)
   }
 }
