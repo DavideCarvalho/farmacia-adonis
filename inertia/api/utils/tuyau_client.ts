9eeff3dd768a7465a54181d46abcc854dcbfa5ce
Davi de Carvalho <davi_carvalho96@hotmail.com>
2025-05-19 21:51:48 -0300

    tuyau superjson

--- inertia/api/utils/tuyau_client.ts
+++ inertia/api/utils/tuyau_client.ts
@@ -1,8 +1,10 @@
 /// <reference path="../../../adonisrc.ts" />
 import { createTuyau } from '@tuyau/client'
 import { api } from '../../../.adonisjs/api'
+import { superjson } from '@tuyau/superjson/plugin'
 
 export const tuyau = createTuyau({
   api,
   baseUrl: 'http://localhost:3333',
+  plugins: [superjson()],
 })
